897a64fd545e9353161b4b6a96b7e37f8e85d834
davideveryday <davidcastrosp80@gmail.com>
2026-01-24 18:16:02 -0300

    style modifications: responsiveness, dark/light theme

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-const content = document.getElementById('content');
-content.innerHTML = `Hello, world!`;
+import "./style/styles.css";
+
+// TO-DO: NAVBAR ref https://timmyomahony.com/projects/pubs-with-a-fire/
+const content = document.getElementById("content");
